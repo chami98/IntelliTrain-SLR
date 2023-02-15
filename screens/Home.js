e1173185122cfb18341ddfd7ca3ec7eb224f349d
chami98 <chamikara98@gmail.com>
2023-02-15 21:08:13 +0530

    Created Home Component

---
+++ screens/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+const Home = () => {
+    return (
+
+        <View style={styles.container}>
+            <Text style={styles.text}>IntelliTrain</Text>
+            <Image
+                source={require('../assets/railway.png')}
+            />
+
+        </View>
+    );
+}
+
+export default Home;
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: 20
+    }
+})

754a4e8a85d7483069c2f1f6c03e160cf8bdeb2d
chami98 <chamikara98@gmail.com>
2023-11-20 23:55:16 +0530

    Added styles

--- screens/Home.js
+++ screens/Home.js
@@ -47,67 +47,76 @@ export default Home;
 
 
 const styles = StyleSheet.create({
+    // Main container style
     container: {
-        marginTop: 65,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f2f2f2',
+        marginTop: 65, // Margin at the top
+        flex: 1, // Take up all available space
+        justifyContent: 'center', // Center children vertically
+        alignItems: 'center', // Center children horizontally
+        backgroundColor: '#f2f2f2', // Light gray background
     },
+    // Container for text elements
     textContainer: {
-        alignItems: 'center',
+        alignItems: 'center', // Center children horizontally
     },
+    // App name style
     appName: {
-        fontSize: 38,
-        fontWeight: 'bold',
-        color: '#3969b7'
+        fontSize: 38, // Large font size
+        fontWeight: 'bold', // Bold text
+        color: '#3969b7' // Blue text color
     },
+    // Slogan style
     slogan: {
-        fontSize: 12,
-        textAlign: 'center',
-        marginBottom: 1,
-        fontStyle: 'italic',
-        color: '#3969b7',
-        fontWeight: 'bold'
+        fontSize: 12, // Small font size
+        textAlign: 'center', // Center text
+        marginBottom: 1, // Margin at the bottom
+        fontStyle: 'italic', // Italic text
+        color: '#3969b7', // Blue text color
+        fontWeight: 'bold' // Bold text
     },
+    // Greeting style
     greeting: {
-        fontSize: 22,
-        color: '#2ecc71',
-        fontWeight: 'bold',
+        fontSize: 22, // Large font size
+        color: '#2ecc71', // Green text color
+        fontWeight: 'bold', // Bold text
     },
+    // Container for buttons
     buttonContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        flex: 1, // Take up all available space
+        alignItems: 'center', // Center children horizontally
+        justifyContent: 'center', // Center children vertically
     },
+    // Row style
     row: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        marginVertical: 20,
+        flexDirection: 'row', // Arrange children in a row
+        justifyContent: 'space-around', // Distribute children evenly with space around them
+        marginVertical: 20, // Vertical margin
     },
+    // Button style
     button: {
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        padding: 20,
-        shadowColor: '#000',
+        alignItems: 'center', // Center children horizontally
+        backgroundColor: '#fff', // White background
+        borderRadius: 10, // Rounded corners
+        padding: 20, // Padding on all sides
+        shadowColor: '#000', // Black shadow
         shadowOffset: {
-            width: 0,
-            height: 1,
+            width: 0, // Shadow width
+            height: 1, // Shadow height
         },
-        shadowOpacity: 0.22,
-        shadowRadius: 2.22,
-        elevation: 3,
-        marginRight: 20,
-        marginLeft: 20,
-        width: 150,
-        height: 150
+        shadowOpacity: 0.22, // Shadow opacity
+        shadowRadius: 2.22, // Shadow radius
+        elevation: 3, // Elevation (for Android)
+        marginRight: 20, // Right margin
+        marginLeft: 20, // Left margin
+        width: 150, // Fixed width
+        height: 150 // Fixed height
     },
+    // Button text style
     buttonText: {
-        marginTop: 10,
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: '#3969b7',
-        textAlign: 'center',
+        marginTop: 10, // Margin at the top
+        fontSize: 16, // Medium font size
+        fontWeight: 'bold', // Bold text
+        color: '#3969b7', // Blue text color
+        textAlign: 'center', // Center text
     },
 });
